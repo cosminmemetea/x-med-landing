03aec0adf3f6206a6e4af40f0016c655d2371672
mXc <cosmin.memetea@gmail.com>
2023-03-03 18:17:35 +0200

    Add logo, center image

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
 
@@ -11,7 +12,7 @@ export default function Home() {
 
       <div className={styles.description}>
         <p>
-         More time for you. Wait Less, Live More. 
+        Where passion meets innovation: creating a healthier world. 
         </p>
         <div>
           <a
@@ -24,25 +25,21 @@ export default function Home() {
         </div>
       </div>
 
-      <div className={styles.center}>
-   
-      </div>
+      {/* <div className={styles.center}> */}
+        <div className ={styles.service} >
+                  <span className={styles.catInfo}> XΞDΞUM CORE  </span>
+                  <span className={styles.cat}>    </span>
+                  <div className={styles.media}> 
+                     <Image className={styles.img}
+                       src={`/imgs/ad.gif`} 
+                       width={350}
+                       height={350}
+                       alt=""
+                       ></Image></div>
+                </div>
+      {/* </div> */}
 
       <div className={styles.grid}>
-        <a
-          href="/team"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        
-          <h2 className={inter.className}>
-            X-CREW <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Get to know the compassionate and dedicated team behind xMΞD, and feel confident in the care you'll receive.
-          </p>
-        </a>
 
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -51,9 +48,23 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={inter.className}>
-           X-MISSION  <span>-&gt;</span>
+           MISSION  <span>-&gt;</span>
           </h2>
-          <p className={inter.className}>Discover the passion and purpose behind xMΞD, and know that you're in good hands with our commitment to simplify your health journey</p>
+          <p className={inter.className}>Discover the passion and purpose that drives us, and know that you're in good hands with our commitment to simplify your health journey.</p>
+        </a>
+
+        <a
+          href="/team"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={inter.className}>
+            PARTNERS <span>-&gt;</span>
+          </h2>
+          <p className={inter.className}>
+            Get to know the compassionate and dedicated team behind, and feel confident in the care you'll receive.
+          </p>
         </a>
 
         <a
@@ -63,10 +74,10 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={inter.className}>
-            X-ROUTE <span>-&gt;</span>
+            ROADMAP <span>-&gt;</span>
           </h2>
           <p className={inter.className}>
-            See how xMΞD is revolutionizing the way you manage your health, and be inspired by our clear vision for the future.
+            See how we plan to revolutionize the way you manage your health, and be inspired by our clear vision for the future.
           </p>
         </a>
       </div>
