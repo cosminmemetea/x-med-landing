8919b7f513904df5466f1c2bbd6850404f054f9d
mXc <cosmin.memetea@gmail.com>
2023-03-04 11:50:17 +0200

    Update core page

--- pages/core.tsx
+++ pages/core.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import Image from 'next/image'
-import generic_styles from '../app/page.module.css'
-import styles from '../styles/pages/core.module.css'
+import styles from '../app/page.module.css'
+import core_styles from '../styles/pages/core.module.css'
 import { Inter } from '@next/font/google'
 import '../app/globals.css'
 
@@ -11,9 +11,9 @@ const inter = Inter({ subsets: ['latin'] })
 const Core = () =>{
   return (
 
-     <main className={generic_styles.main}>
+     <main className={styles.main}>
 
-      <div className={generic_styles.description}>
+      <div className={styles.description}>
         <p>
         XΞDΞUM Coding Academy: Where passion meets innovation. 
         </p>
@@ -32,40 +32,83 @@ const Core = () =>{
         infiniteLoop={true}
         showThumbs={false}
        >
-        <div className={styles.swipItem}>
-          <div className={styles.imgBox}>
+        <div className={core_styles.swipItem}>
+          <div className={core_styles.imgBox}>
             <img src={`/imgs/xyndor.jpg`} alt="Slide 1" />
           </div>
-          <div className={styles.detail}>
+          <div className={core_styles.detail}>
               <h2>Xhann, the Powerhouse Pathfinder</h2>
           </div>
         </div>
-        <div className={styles.swipItem}>
-          <div className={styles.imgBox}>
+        <div className={core_styles.swipItem}>
+          <div className={core_styles.imgBox}>
             <img src={`/imgs/rsz_serph.jpg`} alt="Slide 1" />
           </div>
-          <div className={styles.detail}>
+          <div className={core_styles.detail}>
               <h2>Serph, the Techno Titan</h2>
           </div>
         </div>
-        <div className={styles.swipItem}>
-          <div className={styles.imgBox}>
+        <div className={core_styles.swipItem}>
+          <div className={core_styles.imgBox}>
             <img src={`/imgs/rsz_sun.jpg`} alt="Slide 1" />
           </div>
-          <div className={styles.detail}>
+          <div className={core_styles.detail}>
               <h2>Sun, the Design Dominator</h2>
           </div>
         </div>
-        <div className={styles.swipItem}>
-          <div className={styles.imgBox}>
+        <div className={core_styles.swipItem}>
+          <div className={core_styles.imgBox}>
             <img src={`/imgs/rsz_ciap.jpg`} alt="Slide 1" />
           </div>
-          <div className={styles.detail}>
+          <div className={core_styles.detail}>
               <h2>Al Ciapone, the Cyber Conqueror</h2>
           </div>
         </div>
 
       </Carousel>
+
+       <div className={styles.grid}>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={inter.className}>
+           MISSION  <span>-&gt;</span>
+          </h2>
+          <p className={inter.className}>Discover the passion and purpose that drives us, and know that you're in good hands with our commitment to simplify your health journey.</p>
+        </a>
+
+        <a
+          href="/team"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={inter.className}>
+            PARTNERS <span>-&gt;</span>
+          </h2>
+          <p className={inter.className}>
+            Get to know the compassionate and dedicated team behind, and feel confident in the care you'll receive.
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={inter.className}>
+            ROADMAP <span>-&gt;</span>
+          </h2>
+          <p className={inter.className}>
+            See how we plan to revolutionize the way you manage your health, and be inspired by our clear vision for the future.
+          </p>
+        </a>
+      </div>
 
      </main>
   );
