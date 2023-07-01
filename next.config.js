cfa180994b4a0abbf57d8f9eec66315a606dfc17
mXc <cosmin.memetea@gmail.com>
2023-07-01 11:54:47 +0300

    Try again

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  // other configuration options...
+  output: {
+    // Set the output target to export
+    export: {
+      // Set the path for the exported files (adjust the path according to your needs)
+      path: 'out',
+    },
+  },
+};
