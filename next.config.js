43e73813d25b0f0742bb2bad8d2de61200d84304
mXc <cosmin.memetea@gmail.com>
2023-07-01 12:36:29 +0300

    Remove unecessary steps; nextjs docu said

--- next.config.js
+++ next.config.js
@@ -1,12 +1,25 @@
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//   async generateStaticParams() {
+//     return {
+//       '/': { page: '/' },
+//     };
+//   },
+// };
+
+// module.exports = nextConfig;
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  async generateStaticParams() {
-    return {
-      '/': { page: '/' },
-    };
-  },
-};
-
-module.exports = nextConfig;
+  output: 'export',
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
