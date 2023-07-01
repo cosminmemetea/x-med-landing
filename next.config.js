a18b70e623c5a190c6a7cbc9af1984cfbe788b62
mXc <cosmin.memetea@gmail.com>
2023-07-01 12:00:57 +0300

    Last updates

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  // other configuration options...
-  output: {
-    // Set the output target to export
-    export: {
-      // Set the path for the exported files (adjust the path according to your needs)
-      path: 'out',
-    },
-  },
+  
 };
