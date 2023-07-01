6bea86ca7065b94d9211bb97d40fb61d786e6650
mXc <cosmin.memetea@gmail.com>
2023-07-01 12:44:47 +0300

    Update the workflow and next.config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,10 @@
-// /**
-//  * @type {import('next').NextConfig}
-//  */
-// const nextConfig = {
-//   async generateStaticParams() {
-//     return {
-//       '/': { page: '/' },
-//     };
-//   },
-// };
-
-// module.exports = nextConfig;
-
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
   output: 'export',
   // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-  // trailingSlash: true,
+  trailingSlash: true,
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
 }
